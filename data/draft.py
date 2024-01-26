bc57e56cfca2b998afac843372beb8a3949ca590
lefthand67 <lefthand67@yandex.ru>
2024-01-26 18:08:14 +0500

    updated

--- data/draft.py
+++ data/draft.py
@@ -1,15 +1,35 @@
+import datetime
+import random
+import re
 from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
 
+LIMIT = 20
+
 
 def main():
-    html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
-    bs = BeautifulSoup(html, "html.parser")
+    links = get_links("/wiki/Joseph_Stalin")
 
-    title = bs.find_all(id="text", class_="red")
-    for t in title:
-        print(t.get_text())
+    count = 0
+    while len(links) > 0:
+        new_article = links[random.randint(0, len(links) - 1)].attrs["href"]
+        print(new_article)
+        links = get_links(new_article)
+        count += 1
+        if count == LIMIT:
+            break
+    return 0
 
 
-main()
+def get_links(url):
+    html = urlopen(f"https://en.wikipedia.org{url}")
+    bs = BeautifulSoup(html, "lxml")
+
+    return bs.find("div", {"id": "bodyContent"}).find_all(
+        "a", href=re.compile("^(/wiki/)((?!:).)*$")
+    )
+
+
+if __name__ == "__main__":
+    main()
